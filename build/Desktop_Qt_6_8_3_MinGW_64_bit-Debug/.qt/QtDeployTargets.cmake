7f656a7a1960f7663c893b5f09253f5e0b24871a
misaka-ning <1468549029@qq.com>
2026-03-19 16:13:50 +0800

    Delete build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MusicPlayer_FILE D:/Qt/MusicPlayer/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/MusicPlayer.exe)
-set(__QT_DEPLOY_TARGET_MusicPlayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MusicPlayer_RUNTIME_DLLS D:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Multimedia.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Network.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
